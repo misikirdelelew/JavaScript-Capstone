a1776163184cc96b140788e67ebe17ac18e508e1
Reggeditt <tkodjosarso@gmail.com>
2023-03-16 09:56:47 +0000

    add involvement API interactivity

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import './index.css';
 class ListingsData {
   constructor() {
     this.listings = JSON.parse(window.localStorage.getItem('listings')) || [];
+    this.realtorApiUrl = 'https://realty-in-us.p.rapidapi.com/properties/v3/list';
+    this.baseURLInvAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+    this.involvementAPIAppID = window.localStorage.getItem('involvementAPIAppID') || '';
   }
 
   getProperties = async () => {
@@ -16,16 +19,61 @@ class ListingsData {
       body: '{"limit":20,"offset":0,"baths":{"min":3},"list_price":{"max":900,"min":200},"beds":{"max":3,"min":1},"cats":true,"dogs":true,"state_code":"TX","status":["for_rent"],"type":["condos","condo_townhome_rowhome_coop","condo_townhome","townhomes","duplex_triplex","single_family","multi_family","apartment","condop","coop"],"sort":{"direction":"desc","field":"list_date"}}',
     };
 
-    const response = await fetch('https://realty-in-us.p.rapidapi.com/properties/v3/list', options);
+    const response = await fetch(this.realtorApiUrl, options);
     const data = await response.json();
     this.listings = data.data.home_search.results;
     window.localStorage.setItem('listings', JSON.stringify(this.listings));
-  }
+  };
+
+  createInvolvementTrackerApp = async () => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'text/plain',
+      },
+    };
+    const response = await fetch(`${this.baseURLInvAPI}apps/`, options);
+    const data = await response.text();
+    window.localStorage.setItem('involvementAPIAppID', await data);
+  };
+
+  postLike = (id) => {
+    const options = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        item_id: `${id}`,
+      }),
+    };
+
+    fetch(`${this.baseURLInvAPI}apps/${this.involvementAPIAppID}/likes/`, options)
+      .then((response) => {
+        if (response.ok) {
+          this.updateLikes();
+          return response.json();
+        }
+        throw new Error('Error posting like');
+      })
+      .then((data) => console.log(data));
+  };
+
+  getLikes = async () => {
+    const response = await fetch(`${this.baseURLInvAPI}apps/${this.involvementAPIAppID}/likes/`);
+    const data = await response.json();
+    return data;
+  };
+
+  updateLikes = async () => {
+    const likeCounters = document.querySelectorAll('.likes-count');
+    likeCounters.forEach(async (counter, index) => {
+      const likesData = await this.getLikes();
+      counter.innerHTML = `${likesData[index].likes} likes`;
+    });
+  };
 
   render = (container) => {
     this.listings.forEach((listing, i) => {
-      if (i >= 8) return;
-      // console.log(listing);
+      if (i > 8) return;
       const listingEl = document.createElement('div');
       listingEl.classList.add('listing');
       listingEl.setAttribute('data-id', listing.property_id);
@@ -36,7 +84,7 @@ class ListingsData {
       listingTitle.innerHTML = `
         <h3>Rate: $${listing.list_price || 'not available'} per month</h3>
         <div class="likes-info">
-          <button class="like-btn"></button>
+          <button class="like-btn" id="${listing.property_id}"></button>
           <small class="likes-count">0 likes</small>
         </div>
       `;
@@ -51,9 +99,19 @@ class ListingsData {
       `;
       listingEl.append(listingImg, listingTitle, listingLocation);
       container.appendChild(listingEl);
+
+      const likeBtn = document.querySelector(`[data-id="${listing.property_id}"] .like-btn`);
+      likeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.postLike(e.target.id);
+      });
     });
-  }
+  };
 }
-const listingsContainerEl = document.getElementById('listings');
+
 const listingsData = new ListingsData();
+
+const listingsContainerEl = document.getElementById('listings');
 listingsData.render(listingsContainerEl);
+listingsData.updateLikes();
+// listingsData.createInvolvementTrackerApp();
